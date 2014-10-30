ec1285b2bf482a70ad8d11c49a4aebeefa57ff9c
Noa Rave <noa.rave@berlinonline.de>
2014-10-30 16:08:18 +0100

    extend storage.rb code

--- lib/blog/post.rb
+++ lib/blog/post.rb
@@ -1,13 +1,15 @@
 module Blog
 
   class Post
-    attr_reader :title, :date   # read existing data only
+    attr_reader :title, :date    # read existing data only
+    attr_reader :id
     attr_accessor :content      # read existing data or write new data
 
-    def initialize(title, date) 
+    def initialize(id, title, date) 
       @title = title
       @date = date
       @content = content
+      @id = id
     end
 
   end
--- lib/blog/storage.rb
+++ lib/blog/storage.rb
@@ -6,21 +6,31 @@ module Blog
 
     def initialize(base_dir)
       @base_dir = base_dir
+      yaml_path = ("./resources/test_data/posts.yml")       # find the .yml file
+
+      @yaml = YAML::load(File.open(yaml_path))               # open the yml file and load its contents - results in an array of hashes, one for each post
     end
 
     def get_all_posts()
       
-      yaml_path = ("./resources/test_data/posts.yml")       # find the .yml file
-      
-      yaml = YAML::load(File.open(yaml_path))               # open the yml file and load its contents - results in an array of hashes, one for each post
-      
-      posts = yaml.map do |value|
-        Blog::Post.new(value["title"], value["date"])       # iteration on the array of hashes harvests the value for 'title' and 'date' from each hashmap
-      end                                                   # and uses it as an argument for the Post class which returns a post object 
+                
+           
+      posts = @yaml.map do |value|
+        Blog::Post.new(value["id"], value["title"], value["date"])       # iteration on the array of hashes harvests the value for 'title' and 'date' from each hashmap
+      end                                                                # and uses it as an argument for the Post class which returns a post object 
 
       return posts      # returns an array of post objects 
     end
-  end
+  
+    def get_post_by_id(id)
+       @yaml.map do |value|
+        if value["id"] == id
+          return OpenStruct.new(:title => value["title"])         # the output of this method should be an object, on which the method .title could be called.
+        end                                                  # to do that, an iteration is implemented and if the id value of a Hashmap equals the argument,     
+      end                                                    # a new instance of the OpenStruct class is created, with which an object is generated using the    
+     end                                                     # title value of the correct hash. 
+
+    end
 end
 
 
