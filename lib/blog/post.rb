03eed59263e34820e38100d56b709ee17a9df98e
Noa Rave <noa.rave@berlinonline.de>
2014-11-03 17:29:23 +0100

    code for 'test_storage_write'

--- lib/blog/post.rb
+++ lib/blog/post.rb
@@ -1,13 +1,13 @@
 module Blog
 
   class Post
-    attr_reader :title, :date    # read existing data only
+    attr_accessor :title, :date    # read existing data only
     attr_reader :id
     attr_accessor :content      # read existing data or write new data
 
     def initialize(id, title, date) 
       @title = title
-      @date = date
+      @date = date    
       @content = content
       @id = id
     end
--- lib/blog/storage.rb
+++ lib/blog/storage.rb
@@ -1,36 +1,93 @@
 require 'yaml'
+require 'fileutils'
 
 module Blog
- 
+
   class Storage
 
     def initialize(base_dir)
       @base_dir = base_dir
-      yaml_path = ("./resources/test_data/posts.yml")       # find the .yml file
 
-      @yaml = YAML::load(File.open(yaml_path))               # open the yml file and load its contents - results in an array of hashes, one for each post
+      unless File.exist?(base_dir)
+        FileUtils.mkdir_p(base_dir)
+      end
+
+      @yaml_path = File.join(base_dir, ["/posts.yml"])     # find the .yml file
+      
+      if Array === yaml_load
+        # it's ok
+      else
+        File.open(@yaml_path, "w") do |f| 
+          # let's make an empty array and write it, so the YAML strucure is correct
+          YAML.dump([], f)
+        end
+      end
+    end
+
+    def yaml_load
+      return YAML::load(File.open(@yaml_path, 'r'))    # open the yml file and load its contents - results in an array of hashes, one for each post
     end
 
     def get_all_posts()
-      
-                
-           
-      posts = @yaml.map do |value|
+      posts = yaml_load.map do |value|
         Blog::Post.new(value["id"], value["title"], value["date"])       # iteration on the array of hashes harvests the value for 'title' and 'date' from each hashmap
       end                                                                # and uses it as an argument for the Post class which returns a post object 
 
-      return posts      # returns an array of post objects 
+      return posts                                                       # returns an array of post objects 
     end
-  
+
     def get_post_by_id(id)
-       @yaml.map do |value|
+      yaml_load.map do |value|
         if value["id"] == id
-          return OpenStruct.new(value)         # the output of this method should be an object, on which the method .title could be called.
-        end                                                  # to do that, an iteration is implemented and if the id value of a Hashmap equals the argument,     
-      end                                                    # a new instance of the OpenStruct class is created, with which an object is generated.    
-     end                                                    
+          return Blog::Post.new(value["id"], value["title"], value["date"])     
+          # the output of this method should be an object, on which the method .title could be called. 
+        end                                    # to do that, an iteration is implemented and if the id value of a Hashmap equals the argument,     
+      end                                      # a new instance of the OpenStruct class is created, with which an object is generated.    
+    end                                                    
 
-    end
+    def save(post)
+      all_posts = get_all_posts()
+      post_exists = false
+      all_posts.each do |p| 
+        if p.id == post.id
+          p.title == post.title
+          p.date == post.date
+          p.content == post.content
+          post_exists = true
+        end 
+      end
+
+      if post_exists == false
+        all_posts.push(post)
+      end
+      save_post_markdown(post)
+      yaml_update(all_posts)
+    end 
+
+    def save_post_markdown(post)
+      mdpath = File.join(@base_dir, ["%04d" % post.date.year,
+                                     "%02d" % post.date.month,
+                                     "%02d" % post.date.day,
+                                     post.id + ".md"])  
+      mddir = File.dirname(mdpath)
+      unless File.directory?(mddir)
+        FileUtils.mkdir_p(mddir)
+      end
+      File.open(mdpath, 'w') do |f|
+        f.write (post.content)
+      end
+    end  
+
+    def yaml_update(posts)
+      posts_hashes = posts.map do |post|
+        {'id' => post.id,
+        'title' => post.title,
+        'date' => post.date} 
+      end
+      File.open(@yaml_path, 'w') do |y|
+        YAML.dump(posts_hashes, y)
+      end 
+    end  
+
+  end
 end
-
-
